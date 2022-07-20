efb0597f007273b8afdd14e6ea1d8d0e63183d52
xuitty <ufokun@gmail.com>
2022-07-20 14:27:11 +0800

    first commit

---
+++ D2/src/main/java/tw/edu/hfu/servlet/MyServlet.java
@@ -0,0 +1,33 @@
+package tw.edu.hfu.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public abstract class MyServlet extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		super.init(config);
+	}
+
+}
